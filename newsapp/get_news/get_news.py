8bb2f296621d500534d25760918ab1e31b43f2a2
icesnake72 <icesnake72@gmail.com>
2025-03-26 16:46:15 +0900

    get_news.py 각 카테고리에서 모든 기사들을 가져와 인서트로 수정

--- newsapp/get_news/get_news.py
+++ newsapp/get_news/get_news.py
@@ -1,5 +1,4 @@
 from pymysql import connect
-from dotenv import load_dotenv
 import json, requests, os
 
 
@@ -115,6 +114,7 @@ def select_one_by_source_name(conn, source_name):
   
   
 def select_one_by_category_name(conn, category_name):
+  '''카테고리 이름으로 카테고리 ID를 반환'''
   try:
     cursor = conn.cursor()
     sql = 'select id from category where name = %s'
@@ -157,6 +157,7 @@ def select_one_by_title(conn, title):
   
   
 def get_row_count(conn, table_name):
+  '''테이블의 전체 레코드 수를 반환'''
   try:
     cursor = conn.cursor()
     sql = f'select count(*) from {table_name}'
@@ -169,32 +170,69 @@ def get_row_count(conn, table_name):
     return None
   
   
-def insert_article(conn, url, category_id):  
+def insert_article(conn, config_file, category):  
   try:
+    # articles 테이블의 전체 레코드수 가져오기
     old_count = get_row_count(conn, 'articles')
-    res = requests.get(url)
-    if res.status_code != 200:      
-      print('API 호출 실패: ', res.status_code, url)
-      return
     
-    result = res.json()
-    articles = result['articles']
-    for article in articles:
-      if select_one_by_title(conn, article['title']) is None:
-        source_id = select_one_by_source_name(conn, article['source']['name'])
-        if source_id is None:
-          source_id = insert_source_by_name(conn, article['source']['name'])
+    # 카테고리 이름으로 카테고리 ID 가져오기
+    category_id = select_one_by_category_name(conn, category)
+    
+    # API에서 제공하는 해당 카테고리의 전체 기사수
+    total_results = 0
+    
+    # 페이지당 기사 수
+    cpp = 20
+    
+    # 페이지 번호
+    page = 1
+    
+    # 전체 페이지 수
+    total_page = 0
+    
+    while True:    
+      url = make_url(config_file, category, page)
+      res = requests.get(url)
+      if res.status_code != 200:      
+        print('API 호출 실패: ', res.status_code, url)
+        return
+    
+      result = res.json()
+      total_results = result['totalResults']
+      if total_results == 0:
+        print('데이터가 없습니다.')
+        return
+            
+      articles = result['articles']
+      for article in articles:
+        if select_one_by_title(conn, article['title']) is None:
+          source_id = select_one_by_source_name(conn, article['source']['name'])
+          if source_id is None:
+            source_id = insert_source_by_name(conn, article['source']['name'])
+            
+          sql = '''insert into articles(author, title, description, url, url_to_image, published_at, content, category, source) 
+                  values(%s, %s, %s, %s, %s, %s, %s, %s, %s)'''
+          cursor = conn.cursor()
+          cursor.execute(sql, (article['author'], article['title'], article['description'], article['url'], article['urlToImage'], article['publishedAt'], article['content'], category_id, source_id))
           
-        sql = '''insert into articles(author, title, description, url, url_to_image, published_at, content, category, source) 
-                values(%s, %s, %s, %s, %s, %s, %s, %s, %s)'''
-        cursor = conn.cursor()
-        cursor.execute(sql, (article['author'], article['title'], article['description'], article['url'], article['urlToImage'], article['publishedAt'], article['content'], category_id, source_id))
-    
+      # 첫번째 request에서 전체 페이지 수를 계산
+      if total_page == 0:
+        total_page = total_results // cpp + 1
+      
+      # 마지막 페이지까지 도달하면 종료  
+      if page >= total_page:
+        break
+      
+      # 다음 페이지로 이동
+      page += 1
+      
     conn.commit()
     cursor.close()
     
     new_count = get_row_count(conn, 'articles')
-    print(f'신규 데이터: {new_count-old_count}건이 추가되었습니다.')
+    print("="*30)
+    print(f'카테고리: {category}에서 신규 데이터: {new_count-old_count}건이 추가되었습니다.')
+    print("="*30)
   except Exception as e:
     print(f'Error: {e}')
     
@@ -211,10 +249,10 @@ def test_query(conn, sql):
   cursor.close()
   
 
-def make_url(config_file, category):
+def make_url(config_file, category, page=1):
   with open(config_file, 'r', encoding='utf-8') as f:
     res = json.load(f)
-    url = f"https://newsapi.org/v2/top-headlines?country={res['country']}&category={category}&apiKey={res['news_api_key']}"
+    url = f"https://newsapi.org/v2/top-headlines?country={res['country']}&category={category}&page={page}&apiKey={res['news_api_key']}"
     return url
 
 
@@ -227,7 +265,7 @@ if __name__ == '__main__':
     insert_source(conn, url)
     
     for id, name in select_all_category(conn):
-      url = make_url(CONFIG_FILE, name)
-      insert_article(conn, url, id)    
+      # url = make_url(CONFIG_FILE, name)
+      insert_article(conn, CONFIG_FILE, name)    
         
     close_db(conn)
